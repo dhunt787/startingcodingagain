5d38f3dcf0c6f72754b36f6c129ed9d5195cca9f
Donal Hunt <dhunt787@gmail.com>
2019-04-04 16:05:27 +0100

    adding advanced reduce function example

--- functionalprogramming/advancedReduce/advancesreduce.js
+++ functionalprogramming/advancedReduce/advancesreduce.js
@@ -1,5 +1,17 @@
-import fs from 'fs'
+var fs = require('fs')
+//The functions applied to the string output are executed from left to right. Syntax wise the functions are written the same as var output = fs.readFileSync('data.txt', 'utf8').trim().split.('\n').map etc, its just that are on different lines for presentation reasons. This is called 'Chaining'
+var output = fs.readFileSync('data.txt', 'utf8') //You need to specify the characting encoding, otherwise the read in file will be read out as a line of bytes. utf8 is probably the most familiar character encoding.
+  .trim() //removes line breaks etc at the start of a string
+  .split('\n') // \n is a line break (or carriage return). The split method will split the read in file by line breaks
+  .map(line => line.split('\t')) //converts the objects into a more manageable object. maps every line and then splits it via \t (tab characters). The paramater does not need to be declared because the function is called after the .split() function, so the parameter is the output from that function.
+  .reduce((customers /*object we are constructing*/, line /*What we are iterating*/) => {
+    customers[line[0]] = customers[line[0]] || [ /*This means to use the existing array, if there is one. If not create a new array*/ ] //Creates a new array for each of the fist elements in the output from the .map() function. output will be { 'mark johannsson': [], 'Nikkita Smith': [] }
+    customers[line[0]].push({ //pushes the item at index 0 to the existing or new array.
+      name: line[1],
+      price: line[2],
+      quantity: line[3]
+    })
+    return customers
+  }, {}) //We are using an 'object literal' as our starting point, instead of a number.
 
-var output = fs.readFileSync('data.txt')
-
-console.log('output', output);
+console.log('output', JSON.stringify(output, null, 2)); //We created a JS
